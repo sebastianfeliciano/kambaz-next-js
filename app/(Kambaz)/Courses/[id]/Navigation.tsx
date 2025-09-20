5b5279470d52e86f86f4d0ed7bba01dbc20238f5
Sebastian Feliciano <feliciano.sebastian2004@gmail.com>
2025-09-20 05:18:32 +0200

    Making sidebar unordered list

--- app/(Kambaz)/Courses/[id]/Navigation.tsx
+++ app/(Kambaz)/Courses/[id]/Navigation.tsx
@@ -4,14 +4,16 @@ import Link from "next/link";
 export default function CourseNavigation({ id }: { id: string }) {
   return (
     <div id="wd-courses-navigation">
-      <Link href={`/Courses/${id}/Home`} id="wd-course-home-link">Home</Link><br/>
-      <Link href={`/Courses/${id}/Modules`} id="wd-course-modules-link">Modules</Link><br/>
-      <Link href={`/Courses/${id}/Piazza`} id="wd-course-piazza-link">Piazza</Link><br/>
-      <Link href={`/Courses/${id}/Zoom`} id="wd-course-zoom-link">Zoom</Link><br/>
-      <Link href={`/Courses/${id}/Assignments`} id="wd-course-assignments-link">Assignments</Link><br/>
-      <Link href={`/Courses/${id}/Quizzes`} id="wd-course-quizzes-link">Quizzes</Link><br/>
-      <Link href={`/Courses/${id}/Grades`} id="wd-course-grades-link">Grades</Link><br/>
-      <Link href={`/Courses/${id}/People/Table`} id="wd-course-people-link">People</Link><br/>
+      <ul>
+        <li><Link href={`/Courses/${id}/Home`} id="wd-course-home-link">Home</Link></li>
+        <li><Link href={`/Courses/${id}/Modules`} id="wd-course-modules-link">Modules</Link></li>
+        <li><Link href={`/Courses/${id}/Piazza`} id="wd-course-piazza-link">Piazza</Link></li>
+        <li><Link href={`/Courses/${id}/Zoom`} id="wd-course-zoom-link">Zoom</Link></li>
+        <li><Link href={`/Courses/${id}/Assignments`} id="wd-course-assignments-link">Assignments</Link></li>
+        <li><Link href={`/Courses/${id}/Quizzes`} id="wd-course-quizzes-link">Quizzes</Link></li>
+        <li><Link href={`/Courses/${id}/Grades`} id="wd-course-grades-link">Grades</Link></li>
+        <li><Link href={`/Courses/${id}/People/Table`} id="wd-course-people-link">People</Link></li>
+      </ul>
     </div>
   );
 }
